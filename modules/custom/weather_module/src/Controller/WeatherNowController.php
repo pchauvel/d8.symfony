8d89086c52de34027be166f474819c9bf014f710
pchauvel <pierre.chauvel@actency.fr>
2018-12-17 13:21:14 +0000

    weather now controller

--- modules/custom/weather_module/src/Controller/WeatherNowController.php
+++ modules/custom/weather_module/src/Controller/WeatherNowController.php
@@ -4,11 +4,21 @@ namespace Drupal\weather_module\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class WeatherNowController{
+    const APP_ID = '524901';
     public function content($cityId)
     {
+       $url = sprintf(
+            'https://samples.openweathermap.org/data/2.5/forecast?id=%s&appid=%s',
+            $cityId,
+            self::APP_ID
+        );
+        $result = file_get_contents($url);
+        $json_result = json_decode($result,true);
+        dump($json_result);
+        die;
         return array(
             '#title' => 'Weather',
-            '#markup' => $city,
+            '#markup' => $result,
 
         );
     }
